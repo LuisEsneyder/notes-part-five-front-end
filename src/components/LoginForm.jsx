b95f882ab2b5ae8ab53a7713d5a57c4acef15054
Luis Esneyder Fernández <luisexneider1999@gmail.com>
2023-05-07 19:07:43 -0500

    parte typos

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 const LoginForm = ({ handleLogin }) => {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
@@ -37,5 +38,8 @@ const LoginForm = ({ handleLogin }) => {
       </form>
     </div>
   );
+};
+LoginForm.prototype = {
+  handleLogin: PropTypes.func.isRequired,
 };
 export default LoginForm;
